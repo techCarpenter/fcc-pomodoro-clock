b485af6034f5c7d35b38c0b2655d43e43ac01371
techCarpenter <bdevries95@gmail.com>
2019-09-30 14:24:29 -0400

    Updates

---
+++ precache-manifest.af5347011b0080f8d6981f03cabe0242.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ec83cb561077e9050625eede3e21f6",
+    "url": "/pomodoroclock/index.html"
+  },
+  {
+    "revision": "0c416ef44560b51890ac",
+    "url": "/pomodoroclock/static/css/main.1c793510.chunk.css"
+  },
+  {
+    "revision": "96eee86853b9c6153552",
+    "url": "/pomodoroclock/static/js/2.801e139a.chunk.js"
+  },
+  {
+    "revision": "0c416ef44560b51890ac",
+    "url": "/pomodoroclock/static/js/main.10d0a153.chunk.js"
+  },
+  {
+    "revision": "2c2f4a47bfe844718466",
+    "url": "/pomodoroclock/static/js/runtime-main.3d29201c.js"
+  }
+]);
